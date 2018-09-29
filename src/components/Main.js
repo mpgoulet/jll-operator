15c3a627554ef1862efd049e5d54cad7c6c84169
Matthew Goulet <matthewgoulet@Matthews-MacBook-Pro-6.local>
2018-09-29 09:34:16 -0400

    Working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
-// import Roster from "./Roster";
-// import Schedule from "./Schedule";
+import Roster from "./Roster";
+import Schedule from "./Schedule";
 
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
@@ -13,8 +13,8 @@ const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Home} />
-      {/* <Route path="/roster" component={Roster} />
-      <Route path="/schedule" component={Schedule} /> */}
+      <Route path="/roster" component={Roster} />
+      <Route path="/schedule" component={Schedule} />
     </Switch>
   </main>
 );
